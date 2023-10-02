6938a89c5e11bd8798f6aa7fa98a4b0fe5e8ad15
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2023-10-02 17:19:14 +0200

    Avoid fetching subjects when in SSR mode.

--- frontend/src/scripts/state.ts
+++ frontend/src/scripts/state.ts
@@ -1,9 +1,9 @@
 import { signal } from "@preact/signals";
 import { allSubjects, type Subject } from "./faculties";
 
-export const subjects = await allSubjects(
-  import.meta.env.SITE + "/api/subjects",
-);
+export const subjects = import.meta.env.SSR
+  ? []
+  : await allSubjects(import.meta.env.SITE + "/api/subjects");
 export const subjectsMap = new Map<string, Subject>(
   subjects.map((subject) => [subject.id, subject]),
 );
